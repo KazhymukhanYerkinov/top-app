d59e50981551c052025348a82862d64ce2aba12e
Kazhymukhan <kazhymukhanyerkinov@gmail.com>
2021-11-30 12:03:59 +0600

    done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
+import React from 'react';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <React.Fragment>
+      <Head>
+        <title> MyTop - наш лучший топ </title>
+        <link rel = 'icon' href = '/favicon.ico' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
+  
 }
 
-export default MyApp
+export default MyApp;

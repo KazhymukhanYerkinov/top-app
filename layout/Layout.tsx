5df7bb71bf1f6b8d1d7f0d6a85711be4235e1285
Kazhymukhan Yerkinov <kazhymukhanyerkinov@gmail.com>
2021-12-06 16:21:25 +0600

    done

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -5,6 +5,7 @@ import styles from './Layout.module.css';
 import { Header } from './Header/Header';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
+import { AppContextProvider, IAppContext } from '../context/app.context';
 
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
@@ -20,12 +21,14 @@ const Layout = ({ children }: LayoutProps): JSX.Element => {
   );
 };
 
-export const withLayout = <T extends Record<string, unknown>>(Component: React.FC<T>) => {
-  return function withLayoutComponent(props: T) {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: React.FC<T>) => {
+  return function withLayoutComponent(props: T): JSX.Element {
     return (
-      <Layout>
-        <Component {...props} />
-      </Layout>
+      <AppContextProvider menu = {props.menu} firstCategory = {props.firstCategory}>
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      </AppContextProvider>
     );
   };
 };
--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,10 +1,91 @@
+import React from 'react';
+import cn from 'classnames';
+import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
+import { AppContext } from '../../context/app.context';
 import styles from './Menu.module.css';
+import { firstLevelMenu } from '../../helpers/helpers';
+import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
 
 
 export const Menu = (): JSX.Element => {
+  
+  const router = useRouter();
+  const { menu, firstCategory, setMenu } = React.useContext(AppContext);
+  
+
+  const openSecondLevel = (secondCategory: string) => {
+    setMenu && setMenu(menu.map(m => {
+      if (m._id.secondCategory === secondCategory) {
+        m.isOpened = !m.isOpened;
+      }
+      return m;
+    }));
+  };
+
+  const buildFirstLevel = () => {
+    return (
+      <React.Fragment>
+        {firstLevelMenu.map(menu => (
+          <div key={menu.route}>
+            <Link href={`/${menu.route}`}>
+              <a>
+                <div className={cn(styles.firstLevel, {
+                  [styles.firstLevelActive]: menu.id === firstCategory
+                })}>
+                  {menu.icon}
+                  <span> {menu.name} </span>
+                </div>
+              </a>
+            </Link>
+            {menu.id === firstCategory && buildSecondLevel(menu)}
+          </div>
+        ))}
+      </React.Fragment>
+    );
+  };
+
+  const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
+    return (
+      <div className={styles.secondBlock}>
+        {menu.map(m => {
+          if (m.pages.map(p => p.alias).includes(router.asPath.split('/')[2])) {
+            m.isOpened = true;
+          }
+          return (
+            <div key={m._id.secondCategory}>
+              <div className={styles.secondLevel} onClick={() => openSecondLevel(m._id.secondCategory)}>
+                {m._id.secondCategory}
+              </div>
+              <div className={cn(styles.secondLevelBlock, {
+                [styles.secondLevelBlockOpened]: m.isOpened
+              })}>
+                {buildThirdLevel(m.pages, menuItem.route)}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const buildThirdLevel = (pages: PageItem[], route: string) => {
+    return (
+      pages.map((p, index) => (
+        <Link href={`/${route}/${p.alias}`} key={index}>
+          <a className={cn(styles.thirdLevel, {
+            [styles.thirdLevelActive]: `/${route}/${p.alias}` === router.asPath
+          })}>
+            {p.category}
+          </a>
+        </Link>
+      ))
+    );
+  };
+
   return (
     <div className = {styles.menu}>
-
+      {buildFirstLevel()}
     </div>
   );
 };

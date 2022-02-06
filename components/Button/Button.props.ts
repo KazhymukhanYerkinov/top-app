078cf82158979a6f3142f6d681af37faccb39cc6
KazhymukhanYerkinov <kazhymukhanyerkinov@gmail.com>
2022-02-06 18:58:31 +0600

    dones

--- components/Button/Button.props.ts
+++ components/Button/Button.props.ts
@@ -1,6 +1,7 @@
 import React from "react";
 
-export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+export interface ButtonProps extends Omit<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
+'onAnimationStart' | 'onDragStart' | 'onDragEnd' | 'onDrag' | 'ref'> {
   children: React.ReactNode;
   appearance: 'primary' | 'ghost';
   arrow?: 'right' | 'down' | 'none';
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,21 +1,30 @@
+import React from 'react';
 import cn from 'classnames';
 import ArrowIcon from './arrow.svg';
+import { motion, useMotionValue } from 'framer-motion';
 
 import { ButtonProps } from './Button.props';
 import styles from './Button.module.css';
 
 export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps) => {
-  return (
-    <button className = {cn(styles.button, className, {
-      [styles.primary]: appearance === 'primary',
-      [styles.ghost]: appearance === 'ghost'
-    })} {...props}>
-      { children }
-      { arrow !== 'none' &&
-      <span className = {cn(styles.arrow, { 
-        [styles.down]: arrow === 'down' })}>
-        <ArrowIcon />
-      </span> }
-    </button>
-  );
+	const scale = useMotionValue(1);
+
+	return (
+		<motion.button
+			whileHover={{ scale: 1.05 }}
+			className={cn(styles.button, className, {
+				[styles.primary]: appearance === 'primary',
+				[styles.ghost]: appearance === 'ghost'
+			})}
+			style={{ scale }}
+			{...props}>
+			{children}
+			{arrow !== 'none' &&
+				<span className={cn(styles.arrow, {
+					[styles.down]: arrow === 'down'
+				})}>
+					<ArrowIcon />
+				</span>}
+		</motion.button>
+	);
 };
